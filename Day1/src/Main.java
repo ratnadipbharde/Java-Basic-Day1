900add0f61f6f3c020d4c99cd591f149b00f77db
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-13 20:26:36 +0530

    Added Prog 1.1 program to display any message solution

---
+++ Day1/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+//1.1 Write a program to display any message:
+        Main obj1=new Main();
+        obj1.displayMessage();
+
+    }
+
+    private void displayMessage() {
+        System.out.println("Hello world!");
+    }
+}
